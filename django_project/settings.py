8854f735b980dfcae75cf142bc0cbbc0678faefe
Richard <richard.w.morton86@gmail.com>
2022-10-12 10:45:10 +0200

    email

--- django_project/settings.py
+++ django_project/settings.py
@@ -164,3 +164,4 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
