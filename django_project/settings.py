c314e5986f272de88413aacbbc414401a72de780
Richard <richard.w.morton86@gmail.com>
2022-10-14 12:17:08 +0200

    allowed hosts done

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG',default=False)
-ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['cryptic-reef-51644.herokuapp.com','localhost','127.0.0.1']
 
 SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT',default=True)
 
