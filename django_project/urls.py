88b2aad6ef2e4f857a49673a6017559a53bc5819
Richard <richard.w.morton86@gmail.com>
2022-10-14 11:59:58 +0200

    security

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('siteadmin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
     # path('accounts/',include('accounts.urls')),
     path('',include('pages.urls')),
